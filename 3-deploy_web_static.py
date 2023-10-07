268e8b08d545e6f4ea21e3ebeb261b640757707f
abdelilahboubekri <abdelilahbob@gmail.com>
2023-10-06 18:55:03 -0700

    new ip

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@ import time
 from fabric.api import local
 from fabric.operations import env, put, run
 
-env.hosts = ['44.210.150.159', '35.173.47.15']
+env.hosts = ['54.237.217.84', '3.85.136.124']
 
 
 def do_pack():
