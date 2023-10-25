b7d9fd46ffc23c68f2025019b7584087619c3505
abdelilahboubekri <abdelilah.boubekri@gmail.com>
2023-10-24 21:48:13 -0700

    d

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application """
-
+""" Script that starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,7 +9,7 @@ app.url_map.strict_slashes = False
 @app.route('/')
 def hello_hbnb():
     """ Print Web """
-    return 'Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
@@ -21,7 +20,7 @@ def hbnb():
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """Print a char C followed by the value of the text variable """
+    """ Print a char C followed by the value of the text variable """
     return 'C {}'.format(text.replace('_', ' '))
 
 
